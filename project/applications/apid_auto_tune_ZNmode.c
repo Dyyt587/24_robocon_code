c06c5b4dd7634efbc010be6083d50997b73ce9c6
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2024-05-13 10:48:27 +0800

    version1.0 updata

--- project/applications/apid_auto_tune_ZNmode.c
+++ project/applications/apid_auto_tune_ZNmode.c
@@ -5,84 +5,29 @@
 #include "apid_auto_tune_ZNmode.h"
 #include "apid.h"
 #include "ulog.h"
-/**
-#include <pidautotuner.h>
 
-void setup() {
-
-	PIDAutotuner tuner = PIDAutotuner();
-
-	// Set the target value to tune to
-	// This will depend on what you are tuning. This should be set to a value within
-	// the usual range of the setpoint. For low-inertia systems, values at the lower
-	// end of this range usually give better results. For anything else, start with a
-	// value at the middle of the range.
-	tuner.setTargetInputValue(targetInputValue);
-
-	// Set the loop interval in microseconds
-	// This must be the same as the interval the PID control loop will run at
-	tuner.setLoopInterval(loopInterval);
-
-	// Set the output range
-	// These are the minimum and maximum possible output values of whatever you are
-	// using to control the system (Arduino analogWrite, for example, is 0-255)
-	tuner.setOutputRange(0, 255);
-
-	// Set the Ziegler-Nichols tuning mode
-	// Set it to either PIDAutotuner::ZNModeBasicPID, PIDAutotuner::ZNModeLessOvershoot,
-	// or PIDAutotuner::ZNModeNoOvershoot. Defaults to ZNModeNoOvershoot as it is the
-	// safest option.
-	tuner.setZNMode(PIDAutotuner::ZNModeBasicPID);
-
-	// This must be called immediately before the tuning loop
-	// Must be called with the current time in microseconds
-	tuner.startTuningLoop(micros());
-
-	// Run a loop until tuner.isFinished() returns true
-	long microseconds;
-	while (!tuner.isFinished()) {
-
-		// This loop must run at the same speed as the PID control loop being tuned
-		long prevMicroseconds = microseconds;
-		microseconds = micros();
-
-		// Get input value here (temperature, encoder position, velocity, etc)
-		double input = doSomethingToGetInput();
-
-		// Call tunePID() with the input value and current time in microseconds
-		double output = tuner.tunePID(input, microseconds);
-
-		// Set the output - tunePid() will return values within the range configured
-		// by setOutputRange(). Don't change the value or the tuning results will be
-		// incorrect.
-		doSomethingToSetOutput(output);
-
-		// This loop must run at the same speed as the PID control loop being tuned
-		while (micros() - microseconds < loopInterval) delayMicroseconds(1);
-	}
-
-	// Turn the output off here.
-	doSomethingToSetOutput(0);
-
-	// Get PID gains - set your PID controller's gains to these
-	double kp = tuner.getKp();
-	double ki = tuner.getKi();
-	double kd = tuner.getKd();
-}
-
-void loop() {
-
-	// ...
-}
+/*
+	Set the target value to tune to
+	This will depend on what you are tuning. This should be set to a value within
+	the usual range of the setpoint. For low-inertia systems, values at the lower
+	end of this range usually give better results. For anything else, start with a
+	value at the middle of the range.
 */
-void apid_auto_tune_ZNmode_deinit(apid_t *pid);
+
+static float tune_pid(apid_auto_tune_ZNmode_t *tuner, float input, unsigned long cycle);
+void apid_auto_tune_ZNmode_deinit(apid_t *pid)
+{
+	pid->auto_pid = 0;
+	pid->auto_pid_handler = 0;
+}
+
 void __auto_pid_handle1(apid_t *pid, PID_TYPE cycle)
 {
 	// 覆盖输出
 	// APID_(pid);
 	apid_auto_tune_ZNmode_t *auto_pid = (apid_auto_tune_ZNmode_t *)pid->auto_pid;
-	auto_pid->loopInterval = cycle;
-	pid->parameter.out = tunePID(pid->auto_pid, APID_Get_Present(pid), cycle);
+	// auto_pid->loopInterval = cycle;
+	pid->parameter.out = tune_pid(pid->auto_pid, APID_Get_Present(pid), cycle);
 	// LOG_D(":%f",pid->parameter.out);
 	if (isFinished(auto_pid))
 	{
@@ -92,36 +37,24 @@ void __auto_pid_handle1(apid_t *pid, PID_TYPE cycle)
 	}
 }
 
-void apid_auto_tune_ZNmode_init(apid_t *pid, apid_auto_tune_ZNmode_t *tuner, int tuneCycles)
+void apid_auto_tune_ZNmode_init(apid_t *pid, apid_auto_tune_ZNmode_t *tuner, float targetValue, float low_out_range, float up_out_range, int tuneCycles)
 {
 	pid->auto_pid = tuner;
 	pid->auto_pid_handler = __auto_pid_handle1;
 
-	tuner->targetInputValue = 300;
-	tuner->loopInterval = 10;
+	tuner->targetInputValue = targetValue;
+
 	tuner->znMode = ZNModeLessOvershoot;
 	tuner->cycles = tuneCycles;
-	tuner->min = -20000;
-	tuner->max = 20000;
 	tuner->microseconds = 0;
-	setOutputRange(tuner, -20000, 20000);
-	startTuningLoop(tuner);
+	setOutputRange(tuner, low_out_range, up_out_range);
+	start_tune_loop(tuner);
 }
-void apid_auto_tune_ZNmode_deinit(apid_t *pid)
-{
-	pid->auto_pid = 0;
-	pid->auto_pid_handler = 0;
-}
+
 // Set target input for tuning
 void setTargetInputValue(apid_auto_tune_ZNmode_t *tuner, float target)
 {
 	tuner->targetInputValue = target;
-}
-
-// Set loop interval
-void setLoopInterval(apid_auto_tune_ZNmode_t *tuner, long interval)
-{
-	tuner->loopInterval = interval;
 }
 
 // Set output range
@@ -132,32 +65,24 @@ void setOutputRange(apid_auto_tune_ZNmode_t *tuner, float min, float max)
 }
 
 // Set Ziegler-Nichols tuning mode
-void setZNMode(apid_auto_tune_ZNmode_t *tuner, ZNMode zn)
+void set_zn_mode(apid_auto_tune_ZNmode_t *tuner, ZNMode zn)
 {
 	tuner->znMode = zn;
 }
 
-// Set tuning cycles
-void setTuningCycles(apid_auto_tune_ZNmode_t *tuner, int tuneCycles)
-{
-	tuner->cycles = tuneCycles;
-}
-
 // Initialize all variables before loop
-void startTuningLoop(apid_auto_tune_ZNmode_t *tuner)
+void start_tune_loop(apid_auto_tune_ZNmode_t *tuner)
 {
 	tuner->i = 0;		  // Cycle counter
 	tuner->output = true; // Current output state
 	tuner->outputValue = tuner->maxOutput;
 	tuner->t1 = tuner->t2 = 0;							  // Times used for calculating period
 	tuner->microseconds = tuner->tHigh = tuner->tLow = 0; // More time variables
-	tuner->max = -1000000000000;						  // Max input
-	tuner->min = 1000000000000;							  // Min input
 	tuner->pAverage = tuner->iAverage = tuner->dAverage = 0;
 }
 
 // Run one cycle of the loop
-float tunePID(apid_auto_tune_ZNmode_t *tuner, float input, unsigned long cycle)
+float tune_pid(apid_auto_tune_ZNmode_t *tuner, float input, unsigned long cycle)
 {
 	// Useful information on the algorithm used (Ziegler-Nichols method/Relay method)
 	// http://www.processcontrolstuff.net/wp-content/uploads/2015/02/relay_autot-2.pdf
@@ -210,7 +135,8 @@ float tunePID(apid_auto_tune_ZNmode_t *tuner, float input, unsigned long cycle)
 		// Formula given is Ku = 4d / πa
 		// d is the amplitude of the output signal
 		// a is the amplitude of the input signal
-		float ku = (4.0 * ((tuner->maxOutput - tuner->minOutput) / 2.0)) / (M_PI * (tuner->max - tuner->min) / 2.0);
+		// float ku = (4.0 * ((tuner->maxOutput - tuner->minOutput) / 2.0)) / (M_PI * (tuner->max - tuner->min) / 2.0);
+		float ku = (4.0 * ((tuner->maxOutput - tuner->minOutput))) / (M_PI * (tuner->max - tuner->min));
 
 		// Calculate Tu (period of output oscillations)
 		float tu = tuner->tLow + tuner->tHigh;
@@ -254,8 +180,11 @@ float tunePID(apid_auto_tune_ZNmode_t *tuner, float input, unsigned long cycle)
 
 		// Calculate gains
 		tuner->kp = kpConstant * ku;
-		tuner->ki = (tuner->kp / (tiConstant * tu)) * tuner->loopInterval;
-		tuner->kd = (tdConstant * tuner->kp * tu) / tuner->loopInterval;
+		// tuner->ki = (tuner->kp / (tiConstant * tu)) / cycle;
+		// tuner->kd = (tdConstant * tuner->kp * tu) * cycle;
+
+		tuner->ki = (tuner->kp / (tiConstant * tu));
+		tuner->kd = (tdConstant * tuner->kp * tu);
 
 		// Average all gains after the first two cycles
 		if (tuner->i > 1)
@@ -281,8 +210,14 @@ float tunePID(apid_auto_tune_ZNmode_t *tuner, float input, unsigned long cycle)
 		tuner->ki = tuner->iAverage / (tuner->i - 1);
 		tuner->kd = tuner->dAverage / (tuner->i - 1);
 	}
-
-	return tuner->outputValue;
+	if (tuner->output)
+	{
+		return tuner->outputValue;
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 // Get PID constants after tuning
--- project/applications/main.c
+++ project/applications/main.c
@@ -2,7 +2,7 @@
  * @Author: Dyyt587 67887002+Dyyt587@users.noreply.github.com
  * @Date: 2024-04-12 10:14:08
  * @LastEditors: Dyyt587 67887002+Dyyt587@users.noreply.github.com
- * @LastEditTime: 2024-05-13 00:49:26
+ * @LastEditTime: 2024-05-13 01:15:51
  * @FilePath: \project\applications\main.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -90,10 +90,9 @@ int main(void)
 	// apid_auto_t auto_1;
 	motor_t *motor = motor_get(0);
 	motor_set_speed(0, 100);
-	//auto_pid_init(motor->pid_speed,&auto_1, VECTOR, 200,10);
 	apid_auto_tune_ZNmode_t auto_2;
-	apid_auto_tune_ZNmode_init(motor->pid_speed, &auto_2, 20);
-			motor_set_pos(0, 60);
+	apid_auto_tune_ZNmode_init(motor->pid_speed, &auto_2,300,-20000,20000, 20);
+	motor_set_speed(0, 200);
 
 	while (1)
 	{
