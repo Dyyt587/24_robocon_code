613133f526fceaa42c5447629866986f4dfa50c5
Dyyt587 <67887002+Dyyt587@users.noreply.github.com>
2024-06-27 08:40:52 +0800

    update chassis plan move

--- project/applications/chassis/chassis.c
+++ project/applications/chassis/chassis.c
@@ -1,8 +1,8 @@
 /*
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-03-16 09:10:28
- * @LastEditors: Dyyt587 67887002+Dyyt587@users.noreply.github.com
- * @LastEditTime: 2024-04-04 19:48:48
+ * @LastEditors: Dyyt587 805207319@qq.com
+ * @LastEditTime: 2024-04-28 19:58:40
  * @FilePath: \project\applications\chassis\chassis.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,7 @@
 #include "stddef.h"
 #include "ulog.h"
 #include "apid.h"
+#include "float.h"
 
 apid_t pid_offset;
 int chassis_init(chassis_t *chassis, chassis_ops_t *ops)
@@ -36,14 +37,56 @@ int chassis_set_speed(chassis_t *chassis, chassis_speed_t *data)
     return 0;
 }
 
+void chassis_init_curve(CurveObjectType *curve)
+{
+    curve->targetPos = 0;
+    curve->startPos = 0.0f; // 初始位置
+    curve->currentPos = 0.0f;
+    curve->stepPos = 0.1f; // 位置变化的步长
+    //curve->max_pos = 1500.0f;
+    curve->PosMax = FLT_MAX;  // 最大位置限制
+    curve->PosMin = -FLT_MAX; // 最小位置限制
+    curve->aTimes = 0;               // 当前时间步
+    curve->maxTimes = 500;           // 总时间步，实际使用时需要根据实际情况计算
+    curve->curveMode = CURVE_SPTA;   // 使用S型曲线
+    curve->flexible = 10.f;          // S曲线的柔性因子
+}
+
+void chassis_planning_(CurveObjectType *curve, float *out)
+{
+    if (curve->maxTimes)
+    {
+        mine_plan(curve);
+        *out = curve->currentPos;
+    }
+}
+
+int chassis_set_pos_plan(chassis_t *chassis, chassis_pos_t *data)
+{
+    chassis->target.pos = *data;
+
+    chassis->ops->plan_init(chassis,&chassis->present,&chassis->target);
+    chassis->ops->plan(chassis,&chassis->target);
+    chassis->run_status = CHASSIS_POS;
+
+    LOG_D("target_x %f target_y %f target_w %f",chassis->target.pos.x_m,chassis->target.pos.y_m,chassis->target.pos.z_rad);
+		return 0;
+
+
+}
+int chassis_set_speed_plan(chassis_t *chassis, chassis_speed_t *data)
+{
+	return 0;
+}
 const chassis_pos_t *chassis_get_pos(chassis_t *chassis)
 {
     /* 返回当前位置 */
+
     return &chassis->present.pos;
 }
 void chassis_pos_clean(chassis_t *chassis)
 {
-    chassis->present.pos=chassis->target.pos;
+    chassis->present.pos = chassis->target.pos;
 }
 const chassis_speed_t *chassis_get_speed(chassis_t *chassis)
 {
@@ -100,12 +143,25 @@ rt_weak void chassis_updata_offset(chassis_t *chassis, int time_ms)
     extern float w_z;
     // chassis->offset.pos.x_m = 0.8 * (chassis->target.pos.x_m + (pos_x / 1000.f));
     // chassis->offset.pos.y_m = 0.8 * (chassis->target.pos.y_m - (pos_y / 1000.f));
-    //chassis->offset.pos.z_rad = 0.5 * (chassis->target.pos.z_rad + (zangle * 3.1415926f / 180.f));
+    // chassis->offset.pos.z_rad = 0.5 * (chassis->target.pos.z_rad + (zangle * 3.1415926f / 180.f));
 
-chassis->offset.pos.x_m = 0;
-chassis->offset.pos.y_m = 0;
-chassis->offset.pos.z_rad =0;
+    chassis->offset.pos.x_m = 0;
+    chassis->offset.pos.y_m = 0;
+    chassis->offset.pos.z_rad = 0;
+}
 
+
+void chassis_planning_if(chassis_t *chassis, int time_ms)
+{
+   // chassis_check_planning(chassis);
+    if (chassis->plan.is_planning)
+    {
+        chassis->ops->plan(chassis,&chassis->target);
+        // chassis_planning_(&chassis->plan.curve_x, &chassis->target.pos.x_m);
+        // chassis_planning_(&chassis->plan.curve_y, &chassis->target.pos.y_m);
+        // chassis_planning_(&chassis->plan.curve_w, &chassis->target.pos.z_rad);
+        // chassis->run_status = CHASSIS_POS;
+    }
 }
 
 /**
@@ -117,6 +173,8 @@ chassis->offset.pos.z_rad =0;
  */
 int chassis_handle(chassis_t *chassis, int time_ms)
 {
+	    static int cnt=0;
+
     if (chassis == NULL)
     {
         CHASSIS_LOG("chassis is NULL\n");
@@ -126,10 +184,14 @@ int chassis_handle(chassis_t *chassis, int time_ms)
     chassis_updata_pos(chassis, time_ms);
 
     chassis_updata_offset(chassis, time_ms);
+    chassis_planning_if(chassis, time_ms);
     /* 根据状态执行相应的操作 */
     switch (chassis->run_status)
     {
     case CHASSIS_IDLE:
+    if(cnt++%10==0){
+        CHASSIS_LOG("chassis status IDEL\n",1);
+    }
         break;
     case CHASSIS_SPEED:
         chassis_drv_set_speed(chassis);
--- project/applications/chassis/chassis.h
+++ project/applications/chassis/chassis.h
@@ -1,8 +1,8 @@
 /*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2024-03-16 09:10:35
- * @LastEditors: Dyyt587 67887002+Dyyt587@users.noreply.github.com
- * @LastEditTime: 2024-04-05 23:37:15
+ * @Author: Dyyt587 805207319@qq.com
+ * @Date: 2024-03-30 21:11:21
+ * @LastEditors: Dyyt587 805207319@qq.com
+ * @LastEditTime: 2024-04-28 19:39:03
  * @FilePath: \project\applications\chassis\chassis.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,6 +12,8 @@
 extern "C" {
 #endif
 #include "chassis_cfg.h"
+#include "chassis_planning.h"
+#include "stdbool.h"
 
 #define CHASSIS_LOG(fmt, ...) LOG_D(fmt, ##__VA_ARGS__)
 struct chassis;
@@ -21,15 +23,7 @@ typedef enum chassis_status
     CHASSIS_SPEED,
     CHASSIS_POS,
 } chassis_status;
-typedef struct chassis_ops
-{
-    /*注意 这里负责传输的数据是由driver和module提供，即output由输出者提供，HAL不保存该过程数据，也不会干预*/
-    /*module 负责将底盘的数据转换为车轮(电机)数据，并且通过output输出，将input数据转换为HAL层数据并且更新 */
-    int (*module)(struct chassis *chassis, const void *output, const void *input,chassis_status require_cmd);
-    /*driver 负责将input车轮(电机)的数据进行执行，并且通过output输出车轮(电机)的实时数据 */
-    int (*driver)(struct chassis *chassis, const void *input, const void *output,chassis_status require_cmd);
-    void* date;/* 用于保存相互传递的数据 */
-} chassis_ops_t;
+
 
 typedef struct chassis_speed
 {
@@ -50,20 +44,60 @@ typedef struct chassis_data
     chassis_pos_t pos;
 
 } chassis_data_t;
+typedef enum{
+    CHASSIS_PLAN_NONE,
+    CHASSIS_PLAN_TRAPEZIUM,
+    CHASSIS_PLAN_TYPE_S,
+}chassis_plan_type_e;
+typedef struct chassis_planning chassis_planning_t;
 
+struct chassis_planning{
+    bool is_planning;
+    chassis_data_t plan_start;
+    chassis_data_t plan_end;
 
-typedef struct chassis
+    // CurveObjectType curve_x;
+    // CurveObjectType curve_y;
+
+    CurveObjectType curve_xy;/*xy 方向的向量*/
+    CurveObjectType curve_w;
+
+	float stepPos;  //位置变化步长， 
+	int maxTimes;   //调速跨度,最大执行时间
+	float flexible;     //S曲线拉伸度,柔性因子
+};
+
+typedef struct chassis_ops
 {
+    /*注意 这里负责传输的数据是由driver和module提供，即output由输出者提供，HAL不保存该过程数据，也不会干预*/
+    /*module 负责将底盘的数据转换为车轮(电机)数据，并且通过output输出，将input数据转换为HAL层数据并且更新 */
+    int (*module)(struct chassis *chassis, const void *output, const void *input,chassis_status require_cmd);
+    /*driver 负责将input车轮(电机)的数据进行执行，并且通过output输出车轮(电机)的实时数据 */
+    int (*driver)(struct chassis *chassis, const void *input, const void *output,chassis_status require_cmd);
+    void* date;/* 用于保存相互传递的数据 */
+
+    int (*plan_init)(struct chassis *chassis, chassis_data_t *present, chassis_data_t *target);
+    int (*plan)(struct chassis *chassis, chassis_data_t *outdata);
+} chassis_ops_t;
+typedef struct chassis
+{        
     chassis_data_t target;
     chassis_data_t present;
     chassis_status run_status;
     chassis_ops_t *ops;
     chassis_data_t offset;
+
+    chassis_planning_t plan;
 } chassis_t;
 
 int chassis_init(chassis_t *chassis, chassis_ops_t *ops);
 int chassis_set_pos(chassis_t *chassis, chassis_pos_t *data);
 int chassis_set_speed(chassis_t *chassis, chassis_speed_t *data);
+
+int chassis_set_pos_plan(chassis_t *chassis, chassis_pos_t *data);
+int chassis_set_speed_plan(chassis_t *chassis, chassis_speed_t *data);
+
+int chassis_config_plan(chassis_t *chassis, chassis_speed_t *data);
 
 const chassis_pos_t *chassis_get_pos(chassis_t *chassis);
 const chassis_speed_t *chassis_get_speed(chassis_t *chassis);
